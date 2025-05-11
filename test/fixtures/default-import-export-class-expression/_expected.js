b5016a707e66828c57acc79b013fe524de827f07
OmkarJ13 <oajoshi13@gmail.com>
2025-05-11 11:58:02 +0530

    Add more tests

---
+++ test/fixtures/default-import-export-class-expression/_expected.js
@@ -0,0 +1,4 @@
+const __default_export_module_0 = class {};
+const Foo = __default_export_module_0;
+const foo = new Foo();
+console.log(foo);
---
+++ test/fixtures/default-import-export-function-expression/_config.js
@@ -0,0 +1,4 @@
+export default {
+  description:
+    'should be able to handle default import and export of a function expression',
+};
