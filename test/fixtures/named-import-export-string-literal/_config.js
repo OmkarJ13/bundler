d5db6984b4ce0098f9e7515a63a070830ac10c7e
OmkarJ13 <oajoshi13@gmail.com>
2025-06-07 23:38:11 +0530

    Minor

--- test/fixtures/named-import-export-string-literal/_config.js
+++ test/fixtures/named-import-export-string-literal/_config.js
@@ -1,3 +1,4 @@
 export default {
-  description: 'should be able to handle named imports and exports',
+  description:
+    'should be able to handle named imports and exports which are aliased to string literals',
 };
